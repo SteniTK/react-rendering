6908f44b04d53576f65c6189bfe747a25eb049be
haitham qudaih <haitham.qudaih@tajawal.com>
2022-06-08 11:05:11 +0400

    feat: added react rendered components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Components from './ReactRendering/Components';
 
 // TODO: Add routes to navigate to each folder
 export default function App() {
   return (
-    <Router>
-     <div>
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/users" element={<Users/>} />
-          <Route path="/" element={<Home/>}/>
-        </Routes>
-      </div>
-    </Router>
+    <>
+      {/* <Router>
+        <div>
+          <Routes>
+            <Route path='/about' element={<About />} />
+            <Route path='/users' element={<Users />} />
+          </Routes>
+        </div>
+      </Router> */}
+
+      <Components />
+    </>
   );
 }
 
 function Home() {
-  return <h2>Home</h2>;
+  return;
 }
 
 function About() {
